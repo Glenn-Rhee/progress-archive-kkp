8894d83e31e9a65f7d1fd3ae7576413c619d499a
davywibowo <davywibowo10@gmail.com>
2025-09-25 09:39:21 +0700

    feat:added form change password

---
+++ src/app/change-password/page.tsx
@@ -0,0 +1,29 @@
+import Alert from "@/components/Alert";
+import BackgroundAnimated from "@/components/BackgroundAnimated";
+import FormChangePassword from "@/components/change-password/FormChangePassword";
+import BackButton from "@/components/login/BackButton";
+import BorderEffect from "@/components/login/BorderEffect";
+import HeaderLogin from "@/components/login/HeaderLogin";
+
+export default function ChangePasswordPage() {
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center p-5 relative overflow-hidden"
+      style={{
+        background: "linear-gradient(135deg, #59AC77 0%, #3A6F43 25%,)",
+      }}
+    >
+      <BackButton />
+      <BackgroundAnimated />
+      {/* Login Container */}
+      <div className="relative z-10 w-full max-w-lg">
+        <div className="relative rounded-3xl p-12 overflow-hidden bg-slate-900/50 border border-slate-700/30">
+          <BorderEffect />
+          <HeaderLogin />
+          <Alert />
+          <FormChangePassword />
+        </div>
+      </div>
+    </div>
+  );
+}
